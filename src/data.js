a61196da85246ecfa27eccfc55d221f857c5c5ab
Kostjn04 <kostjn04e@gmail.com>
2025-06-28 20:07:56 +0300

    sdfsdf

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export default [
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: 'Hester Hogsdfsdfsdfsdfan',
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
